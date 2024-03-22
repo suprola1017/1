5bf61ac55eb58c643625daf4e05b8a330a5ffe9c
李傲 <127909349+suprola1017@users.noreply.github.com>
2024-03-22 11:37:13 +0800

    Update convNext.py

--- Physiological_Signal_Encoder/convNeXt/convNext.py
+++ Physiological_Signal_Encoder/convNeXt/convNext.py
@@ -15,31 +15,31 @@ class Self_Attn(nn.Module):
         self.chanel_in = in_dim
         self.activation = activation
         
-        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) #用于计算查询值的1x1卷积层。
-        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) #用于计算键值的1x1卷积层。
-        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1) #用于计算值值的1x1卷积层。
-        self.gamma = nn.Parameter(torch.zeros(1))  #可学习参数，用于缩放自注意力值。
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) 
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim//8 , kernel_size= 1) 
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1) 
+        self.gamma = nn.Parameter(torch.zeros(1))  
 
-        self.softmax  = nn.Softmax(dim=-1) # Softmax函数，沿着最后一个维度（-1）计算注意力权重。
+        self.softmax  = nn.Softmax(dim=-1) 
     def forward(self,x):
         """
             inputs :
-                x : input feature maps( B, C, W, H) 输入特征图的大小为（Batch_size，Channels，Width，Height）。
+                x : input feature maps( B, C, W, H) 
             returns :
                 out : self attention value + input feature 
                 attention: B, N, N (N is Width*Height)
         """
         m_batchsize,C,width ,height = x.size()
-        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B, CX(N)  通过应用查询卷积并整形结果得到的查询值。然后进行转置。
-        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B, C x (*W*H)  通过应用键卷积得到的键值。
-        energy =  torch.bmm(proj_query,proj_key) # transpose check     proj_query和proj_key之间的矩阵乘法
-        attention = self.softmax(energy) # B, (N), (N)               对能量矩阵应用Softmax以获取注意力权重
-        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B, C, N   通过应用值卷积得到的值值。
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B, CX(N) 
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) 
+        energy =  torch.bmm(proj_query,proj_key) # transpose check     
+        attention = self.softmax(energy) # B, (N), (N)     
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B, C, N  
 
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )  #使用注意力权重对proj_value进行加权求和。然后进行整形。
+        out = torch.bmm(proj_value,attention.permute(0,2,1) ) 
         out = out.view(m_batchsize,C,width,height)
         
-        out = self.gamma*out + x  #输出是缩放的注意力加权值与原始输入的和（gamma * out + x）。
+        out = self.gamma*out + x  
         return out
 
 
